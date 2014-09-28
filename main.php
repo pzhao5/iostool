8f3b30072e3e15ba2737937ffc831c2f9ad80a61
Peisen Zhao <pzhao5@uwalumni.com>
2014-09-28 16:26:53 -0400

    Disable debugging

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ $inputFileDir = $argv[1];
 echo "input file dir:" . $inputFileDir . "\n";
 
 $listFile = scandir($inputFileDir, SCANDIR_SORT_ASCENDING);
-$sorter = new HeaderSorter(true);
+$sorter = new HeaderSorter(false);
 foreach ($listFile as $filename) {
   $filepath = $inputFileDir . "/" . $filename;
   $sorter->sort($filepath);
